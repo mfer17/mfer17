3e922cfe25c309025b95df22a6c3cebf48290192
Maria Fernanda Monoga Mejia <desarrolladorweb4@kronotime.com.co>
2025-04-25 17:02:44 -0500

    new changes with structure and images

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,45 @@ const menu = document.getElementById('option-menu-hamburger');
 const iconShow = document.getElementById("menu-hamburger-open");
 const iconClose = document.getElementById("menu-hamburger-close");
 
+const language = document.getElementById('select-language');
+
+
+
+language.addEventListener('change', async (event) => {
+    const elementsHTML = {
+        about: document.querySelectorAll('.item-menu-about'),
+        projects: document.querySelectorAll('.item-menu-projects'),
+        proffessional_experience: document.querySelectorAll('.item-menu-proffessional-experience'),
+        contact: document.querySelectorAll('.item-menu-contact')
+    }
+
+    console.log(event.target.value);
+    //console.log(languages);
+    fetch('./languages/languages.json')
+    .then(response => response.json())  // Convierte la respuesta en un objeto JSON
+    .then(data => {
+        if(data.languages.find(element => element.type == event.target.value) != undefined){
+
+            itemChange(elementsHTML.about, data.languages.find(element => element.type == event.target.value).translation.menu.about);
+            
+            itemChange(elementsHTML.projects, data.languages.find(element => element.type == event.target.value).translation.menu.project);
+
+            itemChange(elementsHTML.proffessional_experience, data.languages.find(element => element.type == event.target.value).translation.menu.professional_experience);
+
+            itemChange(elementsHTML.contact, data.languages.find(element => element.type == event.target.value).translation.menu.contact);
+        }
+    })
+    .catch(error => {
+        console.error('Error al cargar el archivo JSON:', error);
+    });
+})
+
+const itemChange = (elementChange, value) => {
+    elementChange.forEach(element => {
+        element.innerHTML = value
+    });
+}
+
 class MenuHamburgerOpen extends HTMLElement{
     constructor() {
         super();
